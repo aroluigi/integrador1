b90955bd0d65c0816885379890bb63910354fee6
aroluigi <ablatman@gmail.com>
2022-05-20 18:31:26 -0300

    Agregado control de  campos en blanco

--- scripts.js
+++ scripts.js
@@ -1,31 +1,42 @@
 const precio=200;
-//ver cómo guardar referencias en estos nombres
-//var categoria=[document.getElementById('categoria').value];
-//var cantidad=[document.getElementById('cantidad').value];
 
-function nombreValido(){return true}
-function apellidoValido(){return true}
-function eMailValido(){return true}
-function cantidadValida(){return true}
-function resaltarCategoria(c){c.back}
-//function formCompleto() {if(todoBien){return true;}}
+let nombre=document.getElementById('nombre');
+let apellido=document.getElementById('apellido');
+let correo=document.getElementById('correo');
+let categoria=document.getElementById('categoria');
+let cantidad=document.getElementById('cantidad');
+let tarjeta=document.getElementsByClassName('tarjetaDescuento');
+
+function nombreValido(n){if(n.value!=""){return true;}}
+function apellidoValido(a){if(a.value!=""){return true;}}
+function eMailValido(e){if(e.value!=""){return true;}}
+function cantidadValida(c){if(c.value!=""){return true;}}
+
+function validarYMostrar(n, a, e, c){
+    if(nombreValido(n)){
+        if(apellidoValido(a)){
+            if(eMailValido(e)){
+                if(cantidadValida(c)){
+                    mostrarResumen();
+                } else c.focus();
+            } else e.focus();
+        } else a.focus();
+    } else n.focus();
+}
+
+// function resaltarCategoria(c){
+//     if(c.value=='Estudiante'){tarjeta[0].style.backgroundColor='#FFFF99'}
+//     else if(c.value=='Trainee'){tarjeta[1].style.backgroundColor='#FFFF99'}
+//     else if(c.value=='Junior'){tarjeta[2].style.backgroundColor='#FFFF99'}
+// }
 
 function valorCalculado() {
-    if(document.getElementById('categoria').value=='Público general'){vC=precio*parseInt(document.getElementById('cantidad').value)}
-    else if(document.getElementById('categoria').value=='Estudiante'){vC=.2*precio*parseInt(document.getElementById('cantidad').value)}
-    else if(document.getElementById('categoria').value=='Trainee'){vC=.5*precio*parseInt(document.getElementById('cantidad').value)}
-    else if(document.getElementById('categoria').value=='Junior'){vC=.85*precio*parseInt(document.getElementById('cantidad').value)}
+    if(categoria.value =='Público general'){vC=precio*cantidad.value}
+    else if(categoria.value =='Estudiante'){vC=.2*precio*cantidad.value}
+    else if(categoria.value =='Trainee'){vC=.5*precio*cantidad.value}
+    else if(categoria.value =='Junior'){vC=.85*precio*cantidad.value}
     return vC
 }
-
-/*function valorCalculado() {
-    if(categoria[0]=='Público general'){vC=precio*cantidad[0]}
-    else if(categoria[0]=='Estudiante'){vC=.2*precio*cantidad[0]}
-    else if(categoria[0]=='Trainee'){vC=.5*precio*cantidad[0]}
-    else if(categoria[0]=='Junior'){vC=.85*precio*cantidad[0]}
-    return vC
-}
-*/
 
 function actualizarValor(){
     document.getElementById('totalAPagar').value='Total a Pagar: $' + valorCalculado();
@@ -36,7 +47,8 @@ function mostrarResumen(){
     ventanaResumen=window.open("enConstruccion.png","width=300,height=200")
 }
 
-document.getElementById('cantidad').oninput=function() {actualizarValor();};
-document.getElementById('categoria').oninput=function() {c=document.getElementById('categoria').value; actualizarValor(); resaltarCategoria(c);};
-document.getElementById('resumen').onclick=function() {mostrarResumen();};
+cantidad.oninput=function() {actualizarValor();};
+categoria.oninput=function() {actualizarValor(); resaltarCategoria(this);};
 
+document.getElementById('resumen').onclick=function() {validarYMostrar(nombre, apellido, correo, cantidad)
+};
