3ed03ece99eaa55ed27368c27bfd5df3df02f0c8
aroluigi <ablatman@gmail.com>
2022-05-23 18:09:12 -0300

    Validación de nombre y apellido corregida para aceptar letras acentuadas.

--- scripts.js
+++ scripts.js
@@ -16,11 +16,11 @@ let btnBorrar=document.getElementById('borrar');
 let btnResumen=document.getElementById('resumen');
 //------------------------------------------------------------------------------------------------------------------------
 
-//--------------------------------------Validación rudimentaria de campos-------------------------------------------------
-function nombreValido(n){nOK=/^[a-zA-z]+(\s{1}[a-zA-z]+)*$/; if(nOK.test(n.value)){return true;} else return false} //solo letras y espacios
-function apellidoValido(a){aOK=/^[a-zA-z]+(\s{1}[a-zA-z]+)*$/; if(aOK.test(a.value)){return true;} else return false} //solo letras y espacios
+//---------------------------------------------------Validación de campos-------------------------------------------------
+function nombreValido(n){nOK=/^[a-zA-zÀ-ÿ]+(\s{1}[a-zA-zÀ-ÿ]+)*$/; if(nOK.test(n.value)){return true;} else return false}
+function apellidoValido(a){aOK=/^[a-zA-zÀ-ÿ]+(\s{1}[a-zA-zÀ-ÿ]+)*$/; if(aOK.test(a.value)){return true;} else return false}
 function eMailValido(e){mailOK=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/; if(mailOK.test(e.value)){return true;} else return false}
-function cantidadValida(c){cantOK=/^\d+$/; if(cantOK.test(c.value)){return true;} else return false} //uno o más dígitos
+function cantidadValida(c){cantOK=/^\d+$/; if(cantOK.test(c.value)){return true;} else return false}
 //------------------------------------------------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------------------------------------------------
@@ -28,9 +28,9 @@ function validarYMostrar(n, a, e, c){           //Si todos los campos contienen 
     if(nombreValido(n)){                        //datos ingresados para que el usuario los controle, y continúe o cancele la compra.
         if(apellidoValido(a)){                  //Si no, selecciona el primer campo con datos inválidos para su corrección.
             if(eMailValido(e)){
-                if(cantidadValida(c)){console.log('campos validados')
+                if(cantidadValida(c)){
                     s=(c.value>1)? "s":"";
-                    r="Por favor, verifique si la información ingresada es correcta:\n\nYo, " + n.value+" "+ a.value + ", deseo comprar " + c.value +  " entrada"+ s +" de la categoría \""+categoria.value+"\" para la conferencia Bs As, por un precio total de $" +valorCalculado(c,categoria)+ ". Enviar entradas y comprobante de pago a la dirección de eMail "+ e.value; console.log('mensaje generado');
+                    r="Por favor, verifique si la información ingresada es correcta:\n\nYo, " + n.value+" "+ a.value + ", deseo comprar " + c.value +  " entrada"+ s +" de la categoría \""+categoria.value+"\" para la conferencia Bs As, por un precio total de $" +valorCalculado(c,categoria)+ ". Enviar entradas y comprobante de pago a la dirección de eMail "+ e.value;
                     vendidas=confirm(r);
                     return vendidas;
                 } else {c.focus(); c.select();}
